e2406f5f95110ff4ad426828bb1c26db9321f944
Siddhant Swarup Mallick <siddhantswarupmallick@gmail.com>
2022-09-01 01:08:58 +0530

    Code added

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 var sco = localStorage.getItem("score");
-var count = sco.substring(0, sco.indexOf("+"));
-var count1 = sco.substring(sco.indexOf("+"));
+var count = parseInt(sco.substring(0, sco.indexOf("+")),10);
+var count1 = parseInt(sco.substring(sco.indexOf("+")),10);
 if (count < count1) {
   document.querySelector("h1").innerHTML = "You Lost😟😟😟😟";
 } else if (count > count1) {
